a1f66524116f074bef4cd8d0e037e6c635c35f2e
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2023-07-12 15:51:48 +0100

    Displaying default image for images with broken url

--- components/meetups/MeetupDetails.js
+++ components/meetups/MeetupDetails.js
@@ -1,5 +1,13 @@
+import { useEffect, useState } from "react";
 import { ButtonList } from "../common/ButtonList";
 import styles from "./MeetupDetails.module.css";
+
+const imageExists = (src) => {
+  var image = new Image();
+  image.src = src;
+
+  return image.complete && image.height > 0;
+};
 
 export const MeetupDetails = ({
   title,
@@ -9,11 +17,18 @@ export const MeetupDetails = ({
   hideMoreInfo = false,
   actions,
 }) => {
+  const [isImageExists, setIsImageExists] = useState(true);
+
+  useEffect(() => {
+    setIsImageExists(imageExists(image));
+  }, []);
+
   const className = hideMoreInfo ? "details" : "detailsWithMoreInfo";
+  const imageSrc = isImageExists ? image : "/assets/Image_not_available.png";
   return (
     <div className={styles[className]}>
       <h2 className="center">{title}</h2>
-      <img src={image} alt={title} />
+      <img src={imageSrc} alt={title} />
       {!hideMoreInfo ? (
         <div className={styles.infoWrapper}>
           <div className={styles.label}>Address:</div>
