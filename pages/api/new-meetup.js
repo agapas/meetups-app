d6824c00c6c70525962b3f0b8fd9c2f289d97e29
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2023-07-18 18:39:37 +0100

    Used mongoDB for adding new meetups instead of a local file

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,17 +1,28 @@
+import { MongoClient } from "mongodb";
 import fs from "fs";
 import path from "path";
 
+// TODO:
+// - use env for db connection!
+// - add error handling
+
 const handler = async (req, res) => {
   if (req.method === "POST") {
-    const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-    const fileData = fs.readFileSync(filePath, "utf-8");
-    const meetupsArray = JSON.parse(fileData).meetups;
+    const data = req.body;
 
-    const newMeetup = { ...req.body, id: `m${meetupsArray.length + 1}` };
-    const updatedData = { meetups: meetupsArray.concat([newMeetup]) };
+    const uri =
+      "mongodb+srv://tempUser:tempPasswTest123@cluster0.hfdpj4c.mongodb.net/?retryWrites=true&w=majority";
 
-    fs.writeFileSync(filePath, JSON.stringify(updatedData));
-    res.status(201).json({ message: "Meetup created!", meetup: newMeetup });
+    const client = new MongoClient(uri);
+    await client.connect();
+
+    const db = client.db("meetups");
+    const collection = db.collection("meetups");
+
+    await collection.insertOne(data);
+    client.close();
+
+    res.status(201).json({ message: "Meetup created!" });
   } else {
     res.status(200).json({ message: "This works!" });
   }
