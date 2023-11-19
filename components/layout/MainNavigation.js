9234c87c7c693bd9319883c46d9b8a58ab9c55bb
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2023-11-19 15:25:52 +0000

    Showing selected route in the header

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,17 +1,42 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 import styles from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+  const homePath = "/";
+  const newMeetupPath = "/new-meetup";
+
+  const { pathname, isReady } = useRouter();
+  const [selected, setSelected] = useState(homePath);
+
+  useEffect(() => {
+    if (isReady) {
+      setSelected(pathname);
+    }
+  }, [pathname]);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>XYZ Meetups</div>
       <nav>
         <ul>
           <li key="home">
-            <Link href="/">All Meetups</Link>
+            <Link
+              href={homePath}
+              className={homePath === selected ? "selected" : ""}
+            >
+              All Meetups
+            </Link>
           </li>
           <li key="new">
-            <Link href="/new-meetup">Add New Meetup</Link>
+            <Link
+              href={newMeetupPath}
+              className={newMeetupPath === selected ? "selected" : ""}
+            >
+              Add New Meetup
+            </Link>
           </li>
         </ul>
       </nav>
